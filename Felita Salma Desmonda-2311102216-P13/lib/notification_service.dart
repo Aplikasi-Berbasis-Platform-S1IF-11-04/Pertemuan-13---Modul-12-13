4df3c74e105124e69eb93270a26753a0bacde2d2
Zefanya Tarigan <zefanyatarigan98@gmail.com>
2026-06-14 01:39:01 +0700

    Memperbarui folder Felita Salma Desmonda P13

---
+++ Felita Salma Desmonda-2311102216-P13/lib/notification_service.dart
@@ -0,0 +1,37 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class NotificationService {
+  static final FlutterLocalNotificationsPlugin _notificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  static Future<void> init() async {
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
+
+    const InitializationSettings initializationSettings =
+        InitializationSettings(android: initializationSettingsAndroid);
+
+    await _notificationsPlugin.initialize(initializationSettings);
+  }
+
+  static Future<void> showNotification(int counter) async {
+    const AndroidNotificationDetails androidDetails =
+        AndroidNotificationDetails(
+      'counter_channel', 
+      'Counter Notifications',
+      channelDescription: 'Notifikasi ketika counter bertambah',
+      importance: Importance.max,
+      priority: Priority.high,
+    );
+
+    const NotificationDetails platformDetails =
+        NotificationDetails(android: androidDetails);
+
+    await _notificationsPlugin.show(
+      0,
+      'Counter Update',
+      'Nilai counter saat ini: $counter',
+      platformDetails,
+    );
+  }
+}
